0380ed96059f905b9f423b5e75d9eabda4296b12
Jessica Joseph <jessicaajosephh@gmail.com>
2021-04-27 17:11:27 -0400

    changed the index.html.erb in the users views to show.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
-    def index 
-    end
+    # def index 
+    # end
 
     def new
         @user = User.new
@@ -11,12 +11,15 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save 
             session[:user_id] = @user.id 
-            redirect_to users_path
+            redirect_to @user
         else
             render :new
         end
     end
 
+    def show 
+        @user = User.find(params[:id])
+    end
 
     private
 
