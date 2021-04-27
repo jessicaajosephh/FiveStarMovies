09d0bef2586b0aa74057dd63621ad3a18227858b
Jessica Joseph <jessicaajosephh@gmail.com>
2021-04-27 15:18:49 -0400

    added the belongs_to and has_many relationships in the models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
     belongs_to :user
     belongs_to :genre
+    has_many :reviews 
+    has_many :users, through: :reviews 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :movies
+    has_many :reviews 
+    has_many :reviewed_movies, through: :reviews, source: :movie 
+    has_many :genres, through: :movies 
 end
