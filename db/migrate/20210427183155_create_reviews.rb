b4c1aca3207b3eaad5876944a80896b447304207
Jessica Joseph <jessicaajosephh@gmail.com>
2021-04-27 14:32:40 -0400

    generated a resource for reviews

---
+++ db/migrate/20210427183155_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reviews do |t|
+      t.text :content
+      t.string :rating
+      t.integer :user_id
+      t.integer :movie_id
+
+      t.timestamps
+    end
+  end
+end
