1ad2da2b8b1a82a1cfcedd046610849adf676b4b
Jessica Joseph <jessicaajosephh@gmail.com>
2021-04-27 14:26:16 -0400

    generated a resource for movies

---
+++ db/migrate/20210427182533_create_movies.rb
@@ -0,0 +1,15 @@
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :description
+      t.string :movie_length
+      t.string :director
+      t.string :rating
+      t.integer :user_id
+      t.integer :genre_id
+
+      t.timestamps
+    end
+  end
+end
