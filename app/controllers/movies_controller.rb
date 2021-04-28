a7c54d7739af238a7ad4a668047dd913c2f3c913
Jessica Joseph <jessicaajosephh@gmail.com>
2021-04-27 22:26:39 -0400

    added the logic to the create method so that way a new instance of a movie can be created

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,7 +6,12 @@ class MoviesController < ApplicationController
     end
 
     def create 
-        binding.pry
+        @movie = Movie.new(movie_params)
+        if @movie.save 
+            redirect_to movies_path
+        else
+            render :new
+        end
     end
 
     private 
