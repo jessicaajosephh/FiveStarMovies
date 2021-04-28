a70cb088e58a83ff362bc302350c765c83ba298f
Jessica Joseph <jessicaajosephh@gmail.com>
2021-04-28 14:24:10 -0400

    created a show and new method in the genres controller

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,4 +3,12 @@ class GenresController < ApplicationController
     def index 
         @genres = Genre.all.includes(:movies)
     end
+
+    def show 
+        @genre = Genre.find_by_id(params[:id])
+    end
+
+    def new  
+    end
+    
 end
