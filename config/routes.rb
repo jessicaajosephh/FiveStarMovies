e1b6dbeff0fdd2dc6f906955acea13fe5e4b9caf
Jessica Joseph <jessicaajosephh@gmail.com>
2021-04-29 14:19:06 -0400

    removed code in the nested routes for movies

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     resources :movies, only: [:new, :create, :index]
   end
   resources :movies do
-    resources :reviews, only: [:new, :create, :index]
+    resources :reviews
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
