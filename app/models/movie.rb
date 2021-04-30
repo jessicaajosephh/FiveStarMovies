dd0492d51ddfe8d767763d06babe2bfd8cce57ff
Jessica Joseph <jessicaajosephh@gmail.com>
2021-04-30 12:02:47 -0400

    created a genre attributes method in the movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,9 @@ class Movie < ApplicationRecord
     belongs_to :user
     belongs_to :genre
     has_many :reviews 
-    has_many :users, through: :reviews 
+    has_many :users, through: :reviews
+
+    def genre_attributes=(attr)
+        self.genre = Genre.find_or_create_by(attr) if !attr[:name].blank?
+    end
 end
