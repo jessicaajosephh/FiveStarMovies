a82862fbf739674f44525c62ac6fcd683fde7147
Jessica Joseph <jessicaajosephh@gmail.com>
2021-04-30 18:11:03 -0400

    created a scope method in the movies model that will sort all of the movies by alphabetical order

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,8 @@ class Movie < ApplicationRecord
     has_many :reviews 
     has_many :users, through: :reviews
 
+    scope :alpha, -> { order(:title) }
+
     def genre_attributes=(attr)
         self.genre = Genre.find_or_create_by(attr) if !attr[:name].blank?
     end
