d869a535cb65cf7e23ffa53281a64051d328717a
Jessica Joseph <jessicaajosephh@gmail.com>
2021-04-30 18:02:39 -0400

    a user is able to login with google

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,17 @@ class SessionsController < ApplicationController
     end
 
     def omniauth
-        binding.pry
+        @user = User.find_or_create_by(uid: auth[:uid], provider: auth[:provider]) do |u|
+            u.username = auth[:info][:first_name]
+            u.email = auth[:info][:email]
+            u.password = SecureRandom.hex(20)
+        end
+        if @user.valid?
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else
+            redirect_to login_path
+        end
     end
 
     private 
