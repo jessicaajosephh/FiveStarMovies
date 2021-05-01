c9887649a5b4df5387ce3d097c80f3d373a56a2d
Jessica Joseph <jessicaajosephh@gmail.com>
2021-05-01 18:14:26 -0400

    added the capability for a user to upload an image with the movie

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_183556) do
+ActiveRecord::Schema.define(version: 2021_05_01_220616) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 2021_04_30_183556) do
     t.integer "genre_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "reviews", force: :cascade do |t|
