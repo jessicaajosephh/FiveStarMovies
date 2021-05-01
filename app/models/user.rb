b20771615089bdfe293af7b347cfe717b7cc11e0
Jessica Joseph <jessicaajosephh@gmail.com>
2021-04-30 20:08:31 -0400

    refactored the validations in user and movie models so that they are more custom

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
     has_many :reviewed_movies, through: :reviews, source: :movie 
     has_many :genres, through: :movies 
 
-    validates :username, :email, presence: true
+    validates :username, :email, presence: true, uniqueness: true
 end
