feb625d6caa412b9b36541813ea1f34cb22e71ca
Jessica Joseph <jessicaajosephh@gmail.com>
2021-05-01 18:04:48 -0400

    added the neccessary code into the movie model for the paperclip gem to work

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,9 @@ class Movie < ApplicationRecord
     has_many :reviews 
     has_many :users, through: :reviews
 
+    has_attached_file :image, styles: { medium: "400x600#>"}
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+    
     validates :title, presence: true
     validates :description, presence: true, length: { in: 20..500 }
     validates_format_of :movie_length, with: /\A\d{1}:\d{2}\z/, message: "-Please format movie length as 0:00"
