664b3cd0cc45cab478762e560f12a0df8e3964b3
Jessica Joseph <jessicaajosephh@gmail.com>
2021-05-13 10:59:00 -0400

    created a helper method for the review index header

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,10 @@
 module ReviewsHelper
+
+    def review_index_header
+        if @movie
+            "Reviews for #{@movie.title}"
+        else
+            "All Reviews"
+        end
+    end
 end
