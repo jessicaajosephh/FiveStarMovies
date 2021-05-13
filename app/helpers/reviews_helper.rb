dc25cb67da7b848b16e566757856fb402bed7f24
Jessica Joseph <jessicaajosephh@gmail.com>
2021-05-13 11:02:07 -0400

    created a helper method for the header of a new review

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -7,4 +7,12 @@ module ReviewsHelper
             "All Reviews"
         end
     end
+
+    def new_review_header
+        if @review.movie
+            "New Review for #{@review.movie.title}"
+        else
+            "New Review!"
+        end
+    end
 end
