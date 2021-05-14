bd02794888c3155836bd0cb0ad45dd0f407472dd
Jessica Joseph <jessicaajosephh@gmail.com>
2021-05-14 13:12:12 -0400

    removed a space in the index header helper method

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -2,7 +2,7 @@ module MoviesHelper
 
     def index_header
         if @user
-            "#{@user.username} 's Movies:"
+            "#{@user.username}'s Movies:"
         else
             "All Movies" 
         end
